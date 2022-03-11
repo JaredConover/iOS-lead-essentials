19fd8f7d448cd15003f0b5f71c072b3a36b5305c
Jared Conover <conover.j@gmail.com>
2022-03-11 16:07:54 -0500

    move remote feed loader components  to production

---
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,27 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Jared Conover on 2022-03-11.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
+public final class RemoteFeedLoader {
+    private let url: URL
+    private let client: HTTPClient
+
+    public init(url: URL, client: HTTPClient) {
+        self.client = client
+        self.url = url
+    }
+
+    public func load() {
+        client.get(from: url)
+    }
+}
+
