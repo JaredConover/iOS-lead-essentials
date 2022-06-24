fc1d23d47ea989f5ae49009cb8cd4f30a13a0e3c
Jared Conover <conover.j@gmail.com>
2022-06-24 08:57:49 -0400

    Replace magic number with OK_200 static var for extra context

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -61,14 +61,6 @@ public final class RemoteFeedLoader {
 
 
 private class FeedItemsMapper {
-    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
-        guard response.statusCode == 200 else {
-            throw RemoteFeedLoader.Error.invalidData
-        }
-        let root = try JSONDecoder().decode(Root.self, from: data)
-        return root.items.map { $0.feedItem }
-    }
-
     // This represents the root of the JSON object we receive from the api
     struct Root: Decodable {
         let items : [APIItem]
@@ -86,7 +78,19 @@ private class FeedItemsMapper {
             FeedItem(id: id, description: description, location: location, imageURL: image)
         }
     }
+
+    static var OK_200: Int { return 200 }
+
+    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
+        guard response.statusCode == OK_200 else {
+            throw RemoteFeedLoader.Error.invalidData
+        }
+        let root = try JSONDecoder().decode(Root.self, from: data)
+        return root.items.map { $0.feedItem }
+    }
+
+
 }
 
 
-// 32:26
+// 38:46
