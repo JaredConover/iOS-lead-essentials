701c0dc0bba8f0b6ef18ecdff46118a6400d60bd
Jared Conover <conover.j@gmail.com>
2022-06-24 08:48:47 -0400

    Move mapping logic to a new `FeedItemsMapper` class

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -45,9 +45,10 @@ public final class RemoteFeedLoader {
         let httpCompletion: (HTTPClientResult) -> Void = { httpClientResult in
             switch httpClientResult {
             case let .httpClientSuccess(data, response):
-                if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
-                    loadFeedCompletion(.loadFeedSuccess(root.items.map { $0.feedItem }))
-                } else {
+                do {
+                    let items = try FeedItemsMapper.map(data, response)
+                    loadFeedCompletion(.loadFeedSuccess(items))
+                } catch {
                     loadFeedCompletion(.loadFeedFailure(.invalidData))
                 }
             case .httpClientFailure: loadFeedCompletion(.loadFeedFailure(.connectivity))
@@ -58,21 +59,34 @@ public final class RemoteFeedLoader {
     }
 }
 
-struct Root: Decodable {
-    let items : [APIItem]
-}
 
-// We create an API specific item so that impmlentation details from the api don't leak into the higher level abstractions ie: having to specify the key path for imageURL = "image" in the FeedItem. This way the FeedItem has no knowledge of the API
-// A Classicist TDD Approach.. 32:50
-struct APIItem: Decodable {
-    let id: UUID
-    let description: String?
-    let location: String?
-    let image: URL
+private class FeedItemsMapper {
+    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
+        guard response.statusCode == 200 else {
+            throw RemoteFeedLoader.Error.invalidData
+        }
+        let root = try JSONDecoder().decode(Root.self, from: data)
+        return root.items.map { $0.feedItem }
+    }
 
-    var feedItem: FeedItem {
-        FeedItem(id: id, description: description, location: location, imageURL: image)
+    // This represents the root of the JSON object we receive from the api
+    struct Root: Decodable {
+        let items : [APIItem]
+    }
+
+    // We create an API specific item so that impmlentation details from the api don't leak into the higher level abstractions ie: having to specify the key path for imageURL = "image" in the FeedItem. This way the FeedItem has no knowledge of the API
+    // A Classicist TDD Approach.. 32:50
+    struct APIItem: Decodable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let image: URL
+
+        var feedItem: FeedItem {
+            FeedItem(id: id, description: description, location: location, imageURL: image)
+        }
     }
 }
 
-// 27:55
+
+// 32:26
